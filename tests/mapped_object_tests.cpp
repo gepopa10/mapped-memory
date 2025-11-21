8a28d3d89267aed36eba038bad243790f8c64317
gepopa10 <gpopmescu@gmail.com>
2025-11-21 11:43:36 +0100

    added mapped object test with grow in memory version

--- tests/mapped_object_tests.cpp
+++ tests/mapped_object_tests.cpp
@@ -1,4 +1,6 @@
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
 #include "MappedObject.hpp"
 #include "InMemoryMappedRegion.hpp"
 #include "OffsetCalculator.hpp"
@@ -111,6 +113,34 @@ namespace mapped_object_tests
         EXPECT_EQ((*data)[1], 6);
     }
 
+    TEST_F(GIVEN_object_1, WHEN_push_back_multiple_datas_THEN_data_written_to_memory)
+    {
+        {
+            auto &data = obj->data1;
+            data.push_back(1);
+            data.push_back(2);
+
+            EXPECT_EQ(data[0], 1);
+            EXPECT_EQ(data[1], 2);
+        }
+        {
+            auto &data = obj->data2;
+            data.push_back(11);
+            data.push_back(12);
+
+            EXPECT_EQ(data[0], 11);
+            EXPECT_EQ(data[1], 12);
+        }
+        {
+            auto &data = obj->data3;
+            data.push_back(21);
+            data.push_back(22);
+
+            EXPECT_EQ(data[0], 21);
+            EXPECT_EQ(data[1], 22);
+        }
+    }
+
     class GIVEN_object_2 : public GIVEN_InMemoryMappedRegion
     {
     protected:
@@ -192,5 +222,461 @@ namespace mapped_object_tests
 
         EXPECT_EQ((*data)[0], 9);
         EXPECT_EQ((*data)[1], 10);
+    }
+
+    TEST_F(GIVEN_object_2, WHEN_push_back_multiple_datas_THEN_data_written_to_memory)
+    {
+        {
+            auto &data = obj->data1;
+            data.push_back(1);
+            data.push_back(2);
+
+            EXPECT_EQ(data[0], 1);
+            EXPECT_EQ(data[1], 2);
+        }
+        {
+            auto &data = obj->data2;
+            data.push_back(11);
+            data.push_back(12);
+
+            EXPECT_EQ(data[0], 11);
+            EXPECT_EQ(data[1], 12);
+        }
+        {
+            auto &data = obj->data3;
+            data.push_back(21);
+            data.push_back(22);
+
+            EXPECT_EQ(data[0], 21);
+            EXPECT_EQ(data[1], 22);
+        }
+    }
+
+    class GIVEN_object_1_and_2 : public GIVEN_InMemoryMappedRegion
+    {
+    protected:
+        void SetUp() override
+        {
+            GIVEN_InMemoryMappedRegion::SetUp();
+            obj1 = std::make_unique<mapped_object::MappedObject>(0);
+            obj2 = std::make_unique<mapped_object::MappedObject>(1);
+        }
+        std::unique_ptr<mapped_object::MappedObject> obj1;
+        std::unique_ptr<mapped_object::MappedObject> obj2;
+    };
+
+    TEST_F(GIVEN_object_1_and_2, WHEN_push_back_multiple_datas_THEN_data_written_to_memory)
+    {
+        {
+            auto &data = obj1->data1;
+            data.push_back(1);
+            data.push_back(2);
+
+            EXPECT_EQ(data[0], 1);
+            EXPECT_EQ(data[1], 2);
+        }
+        {
+            auto &data = obj1->data2;
+            data.push_back(11);
+            data.push_back(12);
+
+            EXPECT_EQ(data[0], 11);
+            EXPECT_EQ(data[1], 12);
+        }
+        {
+            auto &data = obj1->data3;
+            data.push_back(21);
+            data.push_back(22);
+
+            EXPECT_EQ(data[0], 21);
+            EXPECT_EQ(data[1], 22);
+        }
+        {
+            auto &data = obj2->data1;
+            data.push_back(91);
+            data.push_back(92);
+
+            EXPECT_EQ(data[0], 91);
+            EXPECT_EQ(data[1], 92);
+        }
+        {
+            auto &data = obj2->data2;
+            data.push_back(91);
+            data.push_back(92);
+
+            EXPECT_EQ(data[0], 91);
+            EXPECT_EQ(data[1], 92);
+        }
+        {
+            auto &data = obj2->data3;
+            data.push_back(91);
+            data.push_back(92);
+
+            EXPECT_EQ(data[0], 91);
+            EXPECT_EQ(data[1], 92);
+        }
+    }
+
+    // partial mock to check the nb of call to flush_and_grow only.
+    class MockInMemoryMappedRegion : public InMemoryMappedRegion
+    {
+    public:
+        MockInMemoryMappedRegion(size_t initial_size, size_t &window_bytes)
+            : InMemoryMappedRegion(initial_size, window_bytes)
+        {
+            // Delegate to real implementation by default
+            ON_CALL(*this, flush_and_grow())
+                .WillByDefault([this]()
+                               { InMemoryMappedRegion::flush_and_grow(); });
+        }
+
+        MOCK_METHOD(void, flush_and_grow, (), (override));
+    };
+
+    class GIVEN_MockInMemoryMappedRegion : public ::testing::Test
+    {
+    protected:
+        void SetUp() override
+        {
+            offset::config::window_size = 2;
+            offset::config::nb_objects = 2;
+            window_size_bytes = mapped_object::MappedObject::get_total_data_size_jump() * offset::config::window_size * offset::config::nb_objects;
+            region = new MockInMemoryMappedRegion(window_size_bytes, window_size_bytes);
+            mapped_object::MappedObject::set_mapped_region(region);
+            base = static_cast<char *>(region->get_address());
+        }
+
+        void TearDown() override
+        {
+            delete region;
+            mapped_object::MappedObject::set_mapped_region(nullptr);
+        }
+
+        size_t window_size_bytes;
+        MockInMemoryMappedRegion *region;
+        char *base;
+    };
+
+    class GIVEN_object_1_mock : public GIVEN_MockInMemoryMappedRegion
+    {
+    protected:
+        void SetUp() override
+        {
+            GIVEN_MockInMemoryMappedRegion::SetUp();
+            obj = std::make_unique<mapped_object::MappedObject>(0);
+        }
+        std::unique_ptr<mapped_object::MappedObject> obj;
+    };
+
+    class GIVEN_object_1_data_1_mock : public GIVEN_object_1_mock
+    {
+    protected:
+        void SetUp() override
+        {
+            GIVEN_object_1_mock::SetUp();
+            data = &obj->data1;
+        }
+
+        using data_t = decltype(obj->data1)::underlying_type;
+        decltype(obj->data1) *data;
+    };
+
+    TEST_F(GIVEN_object_1_data_1_mock, WHEN_push_back_thrice_THEN_flush_and_grow_called)
+    {
+        EXPECT_CALL(*region, flush_and_grow())
+            .Times(testing::Exactly(1));
+
+        // Trigger condition that should call flush_and_grow
+        for (size_t i = 0; i < offset::config::window_size + 1; ++i)
+        {
+            data->push_back(i);
+        }
+
+        EXPECT_EQ((*data)[0], 0);
+        EXPECT_EQ((*data)[1], 1);
+        EXPECT_EQ((*data)[2], 2);
+    }
+
+    class GIVEN_object_1_multiple_data_mock : public GIVEN_object_1_mock
+    {
+    protected:
+        void SetUp() override
+        {
+            GIVEN_object_1_mock::SetUp();
+        }
+    };
+
+    TEST_F(GIVEN_object_1_multiple_data_mock, WHEN_push_back_multiple_data_THEN_flush_and_grow_called_once)
+    {
+        EXPECT_CALL(*region, flush_and_grow())
+            .Times(testing::Exactly(1));
+
+        {
+            int diff = 10;
+            auto &data = obj->data1;
+
+            for (size_t i = 0; i < offset::config::window_size; ++i)
+            {
+                data.push_back(i + diff);
+            }
+            EXPECT_EQ(data[0], 0 + diff);
+            EXPECT_EQ(data[1], 1 + diff);
+        }
+
+        // Trigger condition that should call flush_and_grow
+        {
+            int diff = 0;
+            auto &data = obj->data2;
+
+            for (size_t i = 0; i < offset::config::window_size + 1; ++i)
+            {
+                data.push_back(i + diff);
+            }
+            EXPECT_EQ(data[0], 0 + diff);
+            EXPECT_EQ(data[1], 1 + diff);
+            EXPECT_EQ(data[2], 2 + diff);
+        }
+
+        // now these should not trigger grow since the window has already grown
+        obj->data1.push_back(12);
+        EXPECT_EQ(obj->data1[2], 12);
+
+        {
+            int diff = 20;
+            auto &data = obj->data3;
+
+            for (size_t i = 0; i < offset::config::window_size + 2; ++i)
+            {
+                data.push_back(i + diff);
+            }
+            EXPECT_EQ(data[0], 0 + diff);
+            EXPECT_EQ(data[1], 1 + diff);
+            EXPECT_EQ(data[2], 2 + diff);
+            EXPECT_EQ(data[3], 3 + diff);
+        }
+    }
+
+    class GIVEN_object_2_mock : public GIVEN_MockInMemoryMappedRegion
+    {
+    protected:
+        void SetUp() override
+        {
+            GIVEN_MockInMemoryMappedRegion::SetUp();
+            obj = std::make_unique<mapped_object::MappedObject>(1);
+        }
+        std::unique_ptr<mapped_object::MappedObject> obj;
+    };
+
+    class GIVEN_object_2_data_1_mock : public GIVEN_object_2_mock
+    {
+    protected:
+        void SetUp() override
+        {
+            GIVEN_object_2_mock::SetUp();
+            data = &obj->data1;
+        }
+
+        using data_t = decltype(obj->data1)::underlying_type;
+        decltype(obj->data1) *data;
+    };
+
+    TEST_F(GIVEN_object_2_data_1_mock, WHEN_push_back_thrice_THEN_flush_and_grow_called)
+    {
+        EXPECT_CALL(*region, flush_and_grow())
+            .Times(testing::Exactly(1));
+
+        // Trigger condition that should call flush_and_grow
+        for (size_t i = 0; i < offset::config::window_size + 1; ++i)
+        {
+            data->push_back(i);
+        }
+
+        EXPECT_EQ((*data)[0], 0);
+        EXPECT_EQ((*data)[1], 1);
+        EXPECT_EQ((*data)[2], 2);
+    }
+
+    class GIVEN_object_2_multiple_data_mock : public GIVEN_object_2_mock
+    {
+    protected:
+        void SetUp() override
+        {
+            GIVEN_object_2_mock::SetUp();
+        }
+    };
+
+    TEST_F(GIVEN_object_2_multiple_data_mock, WHEN_push_back_multiple_data_THEN_flush_and_grow_called_once)
+    {
+        EXPECT_CALL(*region, flush_and_grow())
+            .Times(testing::Exactly(1));
+
+        {
+            int diff = 10;
+            auto &data = obj->data1;
+
+            for (size_t i = 0; i < offset::config::window_size; ++i)
+            {
+                data.push_back(i + diff);
+            }
+            EXPECT_EQ(data[0], 0 + diff);
+            EXPECT_EQ(data[1], 1 + diff);
+        }
+
+        // Trigger condition that should call flush_and_grow
+        {
+            int diff = 0;
+            auto &data = obj->data2;
+
+            for (size_t i = 0; i < offset::config::window_size + 1; ++i)
+            {
+                data.push_back(i + diff);
+            }
+            EXPECT_EQ(data[0], 0 + diff);
+            EXPECT_EQ(data[1], 1 + diff);
+            EXPECT_EQ(data[2], 2 + diff);
+        }
+
+        // now these should not trigger grow since the window has already grown
+        obj->data1.push_back(12);
+        EXPECT_EQ(obj->data1[2], 12);
+
+        {
+            int diff = 20;
+            auto &data = obj->data3;
+
+            for (size_t i = 0; i < offset::config::window_size + 2; ++i)
+            {
+                data.push_back(i + diff);
+            }
+            EXPECT_EQ(data[0], 0 + diff);
+            EXPECT_EQ(data[1], 1 + diff);
+            EXPECT_EQ(data[2], 2 + diff);
+            EXPECT_EQ(data[3], 3 + diff);
+        }
+    }
+
+    TEST_F(GIVEN_object_2_multiple_data_mock, WHEN_push_back_double_window_plus_one_THEN_flush_and_grow_called_twice)
+    {
+        EXPECT_CALL(*region, flush_and_grow())
+            .Times(testing::Exactly(2));
+
+        {
+            int diff = 0;
+            auto &data = obj->data2;
+
+            for (size_t i = 0; i < offset::config::window_size + 1; ++i)
+            {
+                data.push_back(i + diff);
+            }
+            EXPECT_EQ(data[0], 0 + diff);
+            EXPECT_EQ(data[1], 1 + diff);
+            EXPECT_EQ(data[2], 2 + diff);
+        }
+
+        {
+            int diff = 20;
+            auto &data = obj->data3;
+
+            for (size_t i = 0; i < offset::config::window_size + 3; ++i)
+            {
+                data.push_back(i + diff);
+            }
+            EXPECT_EQ(data[0], 0 + diff);
+            EXPECT_EQ(data[1], 1 + diff);
+            EXPECT_EQ(data[2], 2 + diff);
+            EXPECT_EQ(data[3], 3 + diff);
+            EXPECT_EQ(data[4], 4 + diff);
+        }
+    }
+
+    class GIVEN_object_1_and_2_mock : public GIVEN_MockInMemoryMappedRegion
+    {
+    protected:
+        void SetUp() override
+        {
+            GIVEN_MockInMemoryMappedRegion::SetUp();
+            obj1 = std::make_unique<mapped_object::MappedObject>(0);
+            obj2 = std::make_unique<mapped_object::MappedObject>(1);
+        }
+        std::unique_ptr<mapped_object::MappedObject> obj1;
+        std::unique_ptr<mapped_object::MappedObject> obj2;
+    };
+
+    TEST_F(GIVEN_object_1_and_2_mock, WHEN_push_back_double_window_plus_one_different_object_THEN_flush_and_grow_called_twice)
+    {
+        EXPECT_CALL(*region, flush_and_grow())
+            .Times(testing::Exactly(2));
+
+        {
+            int diff = 0;
+            auto &data = obj1->data2;
+
+            for (size_t i = 0; i < offset::config::window_size + 1; ++i)
+            {
+                data.push_back(i + diff);
+            }
+            EXPECT_EQ(data[0], 0 + diff);
+            EXPECT_EQ(data[1], 1 + diff);
+            EXPECT_EQ(data[2], 2 + diff);
+        }
+
+        {
+            int diff = 20;
+            auto &data = obj2->data3;
+
+            for (size_t i = 0; i < offset::config::window_size + 3; ++i)
+            {
+                data.push_back(i + diff);
+            }
+            EXPECT_EQ(data[0], 0 + diff);
+            EXPECT_EQ(data[1], 1 + diff);
+            EXPECT_EQ(data[2], 2 + diff);
+            EXPECT_EQ(data[3], 3 + diff);
+            EXPECT_EQ(data[4], 4 + diff);
+        }
+    }
+
+    class GIVEN_object_1_and_2_in_memory_region : public GIVEN_InMemoryMappedRegion
+    {
+    protected:
+        void SetUp() override
+        {
+            GIVEN_InMemoryMappedRegion::SetUp();
+            obj1 = std::make_unique<mapped_object::MappedObject>(0);
+            obj2 = std::make_unique<mapped_object::MappedObject>(1);
+        }
+        std::unique_ptr<mapped_object::MappedObject> obj1;
+        std::unique_ptr<mapped_object::MappedObject> obj2;
+    };
+
+    TEST_F(GIVEN_object_1_and_2_in_memory_region, WHEN_push_back_double_window_plus_one_different_object_THEN_memory_data_written_to_memory)
+    {
+        {
+            int diff = 0;
+            auto &data = obj1->data2;
+
+            for (size_t i = 0; i < offset::config::window_size + 1; ++i)
+            {
+                data.push_back(i + diff);
+            }
+            EXPECT_EQ(data[0], 0 + diff);
+            EXPECT_EQ(data[1], 1 + diff);
+            EXPECT_EQ(data[2], 2 + diff);
+        }
+
+        {
+            int diff = 20;
+            auto &data = obj2->data3;
+
+            for (size_t i = 0; i < offset::config::window_size + 3; ++i)
+            {
+                data.push_back(i + diff);
+            }
+            EXPECT_EQ(data[0], 0 + diff);
+            EXPECT_EQ(data[1], 1 + diff);
+            EXPECT_EQ(data[2], 2 + diff);
+            EXPECT_EQ(data[3], 3 + diff);
+            EXPECT_EQ(data[4], 4 + diff);
+        }
     }
 }
